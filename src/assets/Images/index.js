327d44290cfeb474612fa1b97d5f99dcaacb9747
Snehashis Gharai <connectsnehashis22@gmail.com>
2023-09-16 03:20:11 +0530

    noise texture add (FINALLY!!) & body clearance

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -1,22 +1,16 @@
 import Shoe from "../Images/Shoe.png";
-import NoiseTexture from "../Images/Noise-Texture.png";
 import footwear_1 from "../Images/footwear_1.png";
 import footwear_2 from "../Images/footwear_2.png";
 import footwear_3 from "../Images/footwear_3.png";
 import footwear from "../Images/footwear.png";
 import Vector from "../Images/Vector.png";
 import Signin from "../Images/sign-in.png";
-import NoiseTexture2 from "../Images/Noise-Texture2.svg";
-import NoiseTexture3 from "../Images/Noise-Texture3.jpg";
 import womenscat from "../Images/womenscat.png";
 import menscat from "../Images/Menscat.png";
 import kidscat from "../Images/kidscat.png";
 
 export {
   Shoe,
-  NoiseTexture,
-  NoiseTexture2,
-  NoiseTexture3,
   footwear_1,
   footwear_2,
   footwear_3,
