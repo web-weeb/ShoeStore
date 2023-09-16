3045222580ecb5d440fac3d7c67219151d932aec
Purbarun <purbarunmondal@gmail.com>
2023-09-17 01:59:45 +0530

    Product Section 3rd div done with placeholder

--- src/constants/index.js
+++ src/constants/index.js
@@ -56,3 +56,38 @@ export const cards = [
     img: footwear_3,
   },
 ];
+
+export const productMYNTG = [
+  {
+    name: "1",
+    img: "http://via.placeholder.com/317x324",
+  },
+  {
+    name: "2",
+    img: "http://via.placeholder.com/317x324",
+  },
+  {
+    name: "3",
+    img: "http://via.placeholder.com/317x324",
+  },
+  {
+    name: "4",
+    img: "http://via.placeholder.com/317x324",
+  },
+  {
+    name: "5",
+    img: "http://via.placeholder.com/317x324",
+  },
+  {
+    name: "6",
+    img: "http://via.placeholder.com/317x324",
+  },
+  {
+    name: "7",
+    img: "http://via.placeholder.com/317x324",
+  },
+  {
+    name: "8",
+    img: "http://via.placeholder.com/317x324",
+  },
+];
